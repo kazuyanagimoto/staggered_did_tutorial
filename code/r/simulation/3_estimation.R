f752ceb04a894dc6effc29ab601a79be7d049d24
kazuyanagimoto <kazuharu.yanagimmoto@gmail.com>
2023-03-15 23:53:51 +0100

    Setup Environment

---
+++ code/r/simulation/3_estimation.R
@@ -0,0 +1,39 @@
+library(here)
+library(tidyverse)
+library(patchwork)
+
+ls_time = list()
+ls_plot = list()
+
+## 0. Benchmark TWFE
+library(fixest)
+
+
+## 1. Fully-saturated TWFE
+
+# (1-A) Stacked regression
+
+# (1-B) Sun and Abraham
+library(fixest)
+
+
+# (1-C) Wooldridge
+
+## 2. Rolling methods
+
+# (2-A) Callaway-Sant'Anna
+
+# (2-B) de Chaisemartin and D'Haultfoeuille
+
+
+## 3. Inputation methods
+
+# (3-A) Borusyak et al.
+
+# (3-B) Gardner
+
+## Combine All Results
+
+
+
+
