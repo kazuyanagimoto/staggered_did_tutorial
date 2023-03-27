2a1f14b217d8afe48427f33bfa8cda6036fcf190
kazuyanagimoto <kazuharu.yanagimmoto@gmail.com>
2023-03-27 03:20:42 +0200

    review umetani's Simulation

--- code/r/simulation/3_estimation.R
+++ code/r/simulation/3_estimation.R
@@ -5,30 +5,14 @@ library(microbenchmark)
 library(multcomp)
 library(broom)
 # Estimator
-library(fixest) # TWFE, Sun and Abraham
+library(fixest) # TWFE, Sun and Abraham, Wooldridge (Manual implementation)
 library(did) # Callaway-Sant'Anna
 library(DIDmultiplegt) # de Chaisemartin and D'Haultfoeuille
 library(didimputation) # Borusyak, Jaravel, Spiess
+library(did2s) # Gardner
 
 # Data
 load(here("output/r/simulation/1_gen_data/data.rds"))
-data <- data |>
-  mutate(rel_time = t - tr_time)
-
-for (i in 0:27) {
-  v_name <- paste("lag", i, sep ='')
-  data <- data |>
-    mutate(!!v_name := ifelse(rel_time == i, 1, 0))
-}
-
-for (i in 1:27) {
-  v_name <- paste("lead", i, sep ='')
-  data <- data |>
-    mutate(!!v_name := ifelse(rel_time == -i, 1, 0))
-}
-
-data <- data |>
-  mutate(lead1 = 0)
 
 ## 0. Benchmark TWFE
 
@@ -39,11 +23,7 @@ est_naive <- function() {
          coef = ctb[,"Estimate"],
          se = ctb[,"Std. Error"]) |>
     separate(name, into = c(NA, "rel_time"), sep = "::", convert = TRUE) |>
-    add_row(rel_time = as.integer(-1), coef = 0, se = 0) |>
-    mutate(lci = coef - 1.96 * se,
-           rci = coef + 1.96 * se) |>
-    filter(between(rel_time, -5, 5)) |>
-    mutate(val_true = if_else(between(rel_time, -5, 0), 0, rel_time * 0.4))
+    add_row(rel_time = as.integer(-1), coef = 0, se = 0)
 }
 
 ## 1. Fully-saturated TWFE
@@ -62,11 +42,7 @@ est_stack <- function() {
          coef = ctb[,"Estimate"],
          se = ctb[,"Std. Error"]) |>
     separate(name, into = c(NA, "rel_time"), sep = "::", convert = TRUE) |>
-    add_row(rel_time = as.integer(-1), coef = 0, se = 0) |>
-    mutate(lci = coef - 1.96 * se,
-           rci = coef + 1.96 * se) |>
-    filter(between(rel_time, -5, 5)) |>
-    mutate(val_true = if_else(between(rel_time, -5, 0), 0, rel_time * 0.4))
+    add_row(rel_time = as.integer(-1), coef = 0, se = 0)
 }
 
 
@@ -80,17 +56,59 @@ est_sunab <- function() {
           coef = ctb[,"Estimate"],
           se = ctb[,"Std. Error"]) |>
      separate(name, into = c(NA, "rel_time"), sep = "::", convert = TRUE) |>
-     add_row(rel_time = as.integer(-1), coef = 0, se = 0) |>
-     mutate(lci = coef - 1.96 * se,
-            rci = coef + 1.96 * se) |>
-     filter(between(rel_time, -5, 5)) |>
-     mutate(val_true = if_else(between(rel_time, -5, 0), 0, rel_time * 0.4))
+     add_row(rel_time = as.integer(-1), coef = 0, se = 0)
 }
 
 # (1-C) Wooldridge
 
 est_wdrg <- function() {
-  data <- data |> 
+
+  ctb <- feols(y ~ i(rel_time, group, ref = c(-1, 27), ref2 = 3) | t + group,
+              data = data, cluster = "id") |>
+        coeftable()
+
+  wtb <- tibble(
+    group = factor(1:2),
+    wgt = c(mean(data$group == 1), mean(data$group == 2))
+  )
+
+  tibble(name = rownames(ctb),
+          coef = ctb[,"Estimate"],
+          se = ctb[,"Std. Error"]) |>
+     separate(name, into = c(NA, "rel_time", "group"), sep = "::") |>
+     separate(rel_time, into = c("rel_time", NA), sep = ":", convert = TRUE) |>
+     filter(between(rel_time, -5, 5)) |>
+     add_row(rel_time = as.integer(-1), group = "1", coef = 0, se = 0) |>
+     add_row(rel_time = as.integer(-1), group = "2", coef = 0, se = 0) |>
+     left_join(wtb, by = "group") |>
+     group_by(rel_time) |>
+     summarize(coef = weighted.mean(coef, wgt),
+               se = sqrt(weighted.mean(se^2, wgt^2)))
+
+}
+
+
+est_wdrg_original <- function() {
+
+  data_wdrg <- data
+
+  for (i in 0:27) {
+    v_name <- paste("lag", i, sep ='')
+    data_wdrg <- data_wdrg |>
+      mutate(!!v_name := ifelse(rel_time == i, 1, 0))
+  }
+
+  for (i in 1:27) {
+    v_name <- paste("lead", i, sep ='')
+    data_wdrg <- data_wdrg |>
+      mutate(!!v_name := ifelse(rel_time == -i, 1, 0))
+  }
+
+  data_wdrg <- data_wdrg |>
+    mutate(lead1 = 0)
+
+
+  data_wdrg <- data_wdrg |> 
     mutate(across(lead1:lead27, list( g2 = \(x) x * (group == 2))),
            across(lead1:lead27, list( g3 = \(x) x * (group == 3))),
            across(lag0:lag27, list( g2 = \(x) x * (group == 2))),
@@ -99,7 +117,7 @@ est_wdrg <- function() {
   xvars_lead <- names(data)[grep("^lead", names(data))]
   formula <- as.formula(paste0("y", " ~ ", paste(xvars_lag, collapse = "+"), " + ",
                                paste(xvars_lead, collapse = "+"), " | t + group"))
-  mod <- feols(formula, data = data, cluster = "id")
+  mod <- feols(formula, data = data_wdrg, cluster = "id")
   
   list <- list()
   for (i in 0:5) {
@@ -136,6 +154,7 @@ est_wdrg <- function() {
       mutate(val_true = if_else(between(rel_time, -5, 0), 0, rel_time * 0.4))
 }
 
+
 ## 2. Rolling methods
 
 # (2-A) Callaway-Sant'Anna
@@ -149,12 +168,9 @@ est_clsa <- function() {
                       data = data) |> aggte(type = "dynamic")
    tibble(rel_time = mod$egt,
           coef = mod$att.egt,
-          se = mod$se.egt) |>
-     filter(rel_time < 6, rel_time > -6) |>
-     mutate(val_true = if_else(between(rel_time, -5, 0), 0, rel_time * 0.4),
-            lci = coef - 1.96 * se,
-            rci = coef + 1.96 * se)
+          se = mod$se.egt)
 }
+
 
 # (2-B) de Chaisemartin and D'Haultfoeuille
 
@@ -162,73 +178,51 @@ est_dcdh <- function() {
   mod <- DIDmultiplegt::did_multiplegt(df = data,
                                        Y = "y", G = "id", T = "t", D = "is_treated",
                                        dynamic = 5, placebo = 5, brep = 100,
-                                       cluster = "id")
+                                       cluster = "id", parallel = TRUE)
   ests <- mod[grepl("^placebo_|^effect|^dynamic_", names(mod))]
-  data.frame(rel_time = names(ests),
-             coef = as.numeric(ests),
-             se = as.numeric(mod[grepl("^se_placebo|^se_effect|^se_dynamic", names(mod))])) |> 
-    mutate(rel_time = sub("effect", "0", rel_time)) |>
-    mutate(rel_time = sub("placebo_", "-", rel_time)) |>
-    mutate(rel_time = as.integer(sub("dynamic_", "", rel_time))) |>
-    mutate(val_true = if_else(between(rel_time, -5, 0), 0, rel_time * 0.4),
-             lci  = coef - se*1.96,
-             rci = coef + se*1.96)
+  tibble(rel_time = names(ests),
+       coef = as.numeric(ests),
+       se = as.numeric(mod[grepl("^se_*", names(mod))])) |>
+    mutate(rel_time = str_replace(rel_time, "placebo_", "-"),
+           rel_time = str_replace(rel_time, "effect", "0"),
+           rel_time = str_replace(rel_time, "dynamic_", ""),
+           rel_time = as.integer(rel_time))
 }
+
+
+
 
 ## 3. Imputation methods
 
 # (3-A) Borusyak et al.
 
 est_bjs <- function() {
-  data <- data |> rename(dep_var = y)
-  mod <- didimputation::did_imputation(data = data,
-                                       yname = "dep_var", 
-                                       # ynameの引数にyという名前の変数を入れるとエラーになる
+  mod <- data |>
+          rename(dep_var = y) |> # BUG: We cannot use "y" as "yname"
+          didimputation::did_imputation(yname = "dep_var",
                                        gname = "tr_time",
                                        tname = "t",
                                        idname = "id",
                                        horizon = TRUE,
                                        pretrends = -5:-1)
   tibble(rel_time = as.integer(mod$term),
-                coef = mod$estimate,
-                se = mod$std.error) |>
-    filter(rel_time < 6, rel_time > -6) |>
-    mutate(val_true = if_else(between(rel_time, -5, 0), 0, rel_time * 0.4),
-           lci = coef - 1.96 * se,
-           rci = coef + 1.96 * se)
+         coef = mod$estimate,
+         se = mod$std.error)
 }
 
 # (3-B) Gardner
 
 est_gard <- function() {
-  t_d <- as.data.frame(i(data$t))
-  colnames(t_d) <- paste0("t",names(t_d))
-  id_d <- as.data.frame(i(data$id))
-  colnames(id_d) <- paste0("id",names(id_d))
-  data <- bind_cols(data, t_d, id_d)
-  fe_vars <- names(data)[grep("^t[0-9]+|^id[0-9]+", names(data))]
-  temp <- data[,c("y","is_treated",fe_vars)]
-  mod_first <- lm(y ~ . - 1 - is_treated - t1980 - id1, data = subset(temp, is_treated == FALSE))
-  data$pred <- predict(mod_first, newdata = data)
-  data$resid <- data$y - data$pred
-  xvars_lead <- names(data)[grep("^lead", names(data))]
-  xvars_lag <- names(data)[grep("^lag", names(data))]
-  formula <- as.formula(paste0("resid", " ~ ", paste(xvars_lead, collapse = "+"), " + ",
-                               paste(xvars_lag, collapse = "+"), "-1"))
-  ctb <- feols(formula, data = data, cluster = "id") |> coeftable()
-  lead1 <- c(0,0,0,0)
-  ctb <- rbind(ctb, lead1)
-  tibble(rel_time = rownames(ctb),
-         coef = ctb[,"Estimate"],
-         se = ctb[,"Std. Error"]) |>
-    mutate(rel_time = sub("lead", "-", rel_time)) |>
-    mutate(rel_time = as.integer(sub("lag", "", rel_time))) |>
-    filter(rel_time < 6, rel_time > -6) |>
-    mutate(val_true = if_else(between(rel_time, -5, 0), 0, rel_time * 0.4),
-           lci = coef - 1.96 * se,
-           rci = coef + 1.96 * se)
-
+  ctb <- did2s::did2s(data, yname = "y", treatment = "is_treated", cluster_var = "id",
+             first_stage = ~ 0 | id + t,
+             second_stage = ~i(rel_time, ref = c(-1))) |> coeftable()
+  tibble(name = rownames(ctb),
+          coef = ctb[,"Estimate"],
+          se = ctb[,"Std. Error"]) |>
+      separate(name, into = c(NA, "rel_time"), sep = "::", convert = TRUE) |>
+      add_row(rel_time = as.integer(-1), coef = 0, se = 0)
 }
+
 
 #-------------------------------------------
 # Plot & Benchmark
@@ -236,26 +230,29 @@ est_gard <- function() {
 
 ## Plot
 plot_did <- function(ctb, title) {
-  ctb |> ggplot(aes(x = rel_time, y = coef, ymin = lci, ymax = rci)) +
-    geom_point(size = 2, color = "skyblue") +
-    geom_line(color = "skyblue") +
-    geom_line(aes(x = rel_time, y = val_true), linetype = "longdash", color = "orange") +
-    geom_vline(xintercept = 0.0, linetype = "longdash") +
-    geom_hline(yintercept = 0.0) +
-    geom_ribbon(alpha = 0.2, fill = "skyblue") +
-    scale_x_continuous(breaks = -5:5) +
-    labs(x = "Periods since the event",
-         y = "Estimates",
-         title = title) +
-    theme_minimal() +
-    theme(panel.grid.major = element_blank(),
-          panel.grid.minor = element_blank(),
-          plot.title.position = "plot")
+  ctb |>
+    filter(between(rel_time, -5, 5)) |>
+    mutate(val_true = if_else(between(rel_time, -5, 0), 0, rel_time * 0.4),
+           lci = coef - 1.96 * se,
+           rci = coef + 1.96 * se) |>
+    ggplot(aes(x = rel_time, y = coef, ymin = lci, ymax = rci)) +
+      geom_point(size = 2, color = "skyblue") +
+      geom_line(color = "skyblue") +
+      geom_line(aes(x = rel_time, y = val_true), linetype = "longdash", color = "orange") +
+      geom_vline(xintercept = 0.0, linetype = "longdash") +
+      geom_hline(yintercept = 0.0) +
+      geom_ribbon(alpha = 0.2, fill = "skyblue") +
+      scale_x_continuous(breaks = -5:5) +
+      labs(x = "Periods since the event",
+          y = "Estimates",
+          title = title) +
+      theme_minimal() +
+      theme(panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank(),
+            plot.title.position = "plot")
 }
 
-p1 <- est_naive()
-
-|> plot_did("Naive TWFE Event Study")
+p1 <- est_naive() |> plot_did("Naive TWFE Event Study")
 p2 <- est_stack() |> plot_did("Stacked Regression")
 p3 <- est_sunab() |> plot_did("Sun and Abraham")
 p4 <- est_wdrg() |> plot_did("Wooldridge")
@@ -269,15 +266,22 @@ ggsave(here("output/r/simulation/3_estimation/alt_est1.pdf"), width = 6, height 
 (p5 + p6) / (p7 + p8)
 ggsave(here("output/r/simulation/3_estimation/alt_est2.pdf"), width = 6, height = 6)
 
+
+
 ## Benchmark
 
 mbm <- microbenchmark(
     sunab = est_sunab(),
     wdrg = est_wdrg(),
     clsa = est_clsa(),
-    dcdh = est_dcdh(),
     bjs = est_bjs(),
-    gard = est_gard()
+    gard = est_gard(),
+    times = 100
+)
+
+mbm_dcdh <- microbenchmark(
+  dcdh = est_dcdh(),
+  times = 1
 )
 
 # Yanagimoto will write here some exporting function of the benchmarks
